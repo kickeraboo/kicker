295ce13335607766fb181c35b4c367a042edfc0b
hescano <hanlet.escano@gmail.com>
2014-03-20 09:43:44 -0600

    Created Base DataService class. Created UserDataService class.

---
+++ DAL/src/dal/DataServiceBase.java
@@ -0,0 +1,9 @@
+package dal;
+
+import java.sql.ResultSet;
+
+public abstract class DataServiceBase {
+	public ResultSet getData(String procedureName) {
+		return null;
+	}
+}
---
+++ DAL/src/dal/UserDataService.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package dal;
+
+import java.util.List;
+
+import bll.User;
+
+/**
+ * @author Hamlet
+ * 
+ */
+public class UserDataService extends DataServiceBase {
+
+	public static List<User> getAllUsers() {
+		return null;
+	}
+}
