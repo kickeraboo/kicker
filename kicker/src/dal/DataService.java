cf91a2720e92084656afd958240118c3556c5dbc
hescano <hanlet.escano@gmail.com>
2014-03-21 09:19:22 -0600

    Loading property files configurations.

--- kicker/src/dal/DataService.java
+++ kicker/src/dal/DataService.java
@@ -1,5 +1,7 @@
 package dal;
 
+import helpers.PropertiesHelper;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -12,68 +14,74 @@ import com.sun.rowset.CachedRowSetImpl;
 
 public class DataService
 {
-	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-	static final String DB_URL = "jdbc:mysql://localhost/kickeraboo";
-
-	// Database credentials
-	static final String USER = "root";
-	static final String PASS = "k1ck3rab00";
-
-	public static ResultSet getData(String query)
-	{
-		Connection conn = null;
-		Statement stmt = null;
-		try
-		{
-			Class.forName("com.mysql.jdbc.Driver");
-			conn = DriverManager.getConnection(DB_URL, USER, PASS);
-			stmt = conn.createStatement();
-			ResultSet rs = stmt.executeQuery(query);
-			
-			//get the cached row set
-			CachedRowSet rowset = new CachedRowSetImpl();
-			rowset.populate(rs);
-			
-			//clean up
-			rs.close();
-			stmt.close();
-			conn.close();
-			
-			//return it
-			return rowset;
-		} 
-		catch (SQLException se)
-		{
-			// Handle errors for JDBC
-			se.printStackTrace();
-		} catch (Exception e)
-		{
-			// Handle errors for Class.forName
-			e.printStackTrace();
-		} finally
-		{
-			// finally block used to close resources
-			try
-			{
-				if (stmt != null)
-					stmt.close();
-			} catch (SQLException se2)
-			{
-			}// nothing we can do
-			try
-			{
-				if (conn != null)
-					conn.close();
-			} catch (SQLException se)
-			{
-				se.printStackTrace();
-			}// end finally try
-		}// end try
-		return null;
-	}
-
-	public static boolean execute(String procedureName)
-	{
-		return false;
-	}
+   
+   // Database credentials
+   static final String USER = PropertiesHelper.getProperty("DB_USER");
+   static final String PASS = PropertiesHelper.getProperty("DB_PASS");
+   static final String JDBC_DRIVER = PropertiesHelper
+         .getProperty("JDBC_DRIVER");
+   static final String DB_URL = PropertiesHelper
+         .getProperty("DB_CONNECTION_STRING");
+   
+   public static ResultSet getData(String query)
+   {
+      Connection conn = null;
+      Statement stmt = null;
+      try
+      {
+         Class.forName(JDBC_DRIVER);
+         conn = DriverManager.getConnection(DB_URL, USER, PASS);
+         stmt = conn.createStatement();
+         ResultSet rs = stmt.executeQuery(query);
+         
+         // get the cached row set
+         CachedRowSet rowset = new CachedRowSetImpl();
+         rowset.populate(rs);
+         
+         // clean up
+         rs.close();
+         stmt.close();
+         conn.close();
+         
+         // return it
+         return rowset;
+      }
+      catch (SQLException se)
+      {
+         // Handle errors for JDBC
+         se.printStackTrace();
+      }
+      catch (Exception e)
+      {
+         // Handle errors for Class.forName
+         e.printStackTrace();
+      }
+      finally
+      {
+         // finally block used to close resources
+         try
+         {
+            if (stmt != null)
+               stmt.close();
+         }
+         catch (SQLException se2)
+         {
+         }// nothing we can do
+         try
+         {
+            if (conn != null)
+               conn.close();
+         }
+         catch (SQLException se)
+         {
+            se.printStackTrace();
+         }// end finally try
+      }// end try
+      return null;
+   }
+   
+   public static boolean execute(String procedureName)
+   {
+      return false;
+   }
 }
--- kicker/src/testServlet.java
+++ kicker/src/testServlet.java
@@ -1,5 +1,4 @@
 
-
 import java.io.IOException;
 import java.util.List;
 
@@ -15,33 +14,40 @@ import bll.User;
  * Servlet implementation class testServlet
  */
 @WebServlet("/testServlet")
-public class testServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public testServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		List<User> users = User.getAll();
-		
-		System.out.println(users.get(0).getEmail());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
-
+public class testServlet extends HttpServlet
+{
+   private static final long serialVersionUID = 1L;
+   
+   /**
+    * @see HttpServlet#HttpServlet()
+    */
+   public testServlet()
+   {
+      super();
+      // TODO Auto-generated constructor stub
+   }
+   
+   /**
+    * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+    *      response)
+    */
+   protected void doGet(HttpServletRequest request, HttpServletResponse response)
+         throws ServletException, IOException
+   {
+      // TODO Auto-generated method stub
+      List<User> users = User.getAll();
+      
+      System.out.println(users.get(0).getEmail());
+   }
+   
+   /**
+    * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+    *      response)
+    */
+   protected void doPost(HttpServletRequest request,
+         HttpServletResponse response) throws ServletException, IOException
+   {
+      // TODO Auto-generated method stub
+   }
+   
 }
